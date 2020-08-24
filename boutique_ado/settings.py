01eb9b510f948fe2b259706443632ba5ff8dd9cd
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-08-24 18:20:50 +0000

    Completed homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -84,7 +84,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID=1
+SITE_ID = 1
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
@@ -147,3 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
